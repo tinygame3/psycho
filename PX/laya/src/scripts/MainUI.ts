8a1e79936c2919fd69d47baf1aa9a9f9822e4a2c
jian yang <fondiyass@163.com>
2019-06-23 10:38:51 +0800

    add res

--- PX/laya/src/scripts/MainUI.ts
+++ PX/laya/src/scripts/MainUI.ts
@@ -1,7 +1,10 @@
-export default class MainUI extends Laya.Scene {
+  export default class MainUI extends Laya.Scene {
     private bg:Laya.Sprite;
+    public btlHouse:Laya.Button;
+    public btlWorld:Laya.Button;
+    public btlFriends:Laya.Button;
     constructor() { 
-        super(); 
+        super();
         this.init();
     }
     
@@ -9,5 +12,19 @@ export default class MainUI extends Laya.Scene {
         this.bg = new Laya.Sprite()
         this.bg.loadImage("res/bg/bg_newyear.png");
         this.addChild(this.bg);
+    }
+    onEnable(): void {
+        this.btlHouse.on(Laya.Event.CLICK, this, this.onHouseClick);
+        this.btlWorld.on(Laya.Event.CLICK, this, this.onWorldClick);
+        this.btlFriends.on(Laya.Event.CLICK, this, this.onFriendsClick);
+    }
+    onHouseClick(e: Laya.Event):void{
+        this.btlHouse.label = "pressed";
+    }
+    onWorldClick(e: Laya.Event):void{
+        this.btlWorld.label = "pressed";
+    }
+    onFriendsClick(e: Laya.Event):void{
+        this.btlFriends.label = "pressed";
     }
 }
